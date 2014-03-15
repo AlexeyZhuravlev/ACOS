cda5534bf96592d7adaae0d5c3a02c6fa9804742
Alexey Zhuravlev <zhuravlev@hill.cs.msu.ru>
2014-03-16 01:52:59 +0400

    Corrected mistakes

--- task-4/main.c
+++ task-4/main.c
@@ -7,7 +7,7 @@
 
 int main(int argc, char** argv)
 {
-   int lake[2];
+   int lake1[2], lake2[2];
    pid_t prog1_id, prog2_id, wc_id;
    int status1, status2, status3;
    if (argc != 10) 
@@ -15,7 +15,7 @@ int main(int argc, char** argv)
        fprintf(stderr, "Not enough arguments\n");
        return 1;
    }
-   if (pipe(lake))
+   if (pipe(lake1) || pipe(lake2))
    {
        fprintf(stderr, "Unable to create pipe\n");
        return 2;
@@ -23,33 +23,41 @@ int main(int argc, char** argv)
    prog1_id = fork();
    if (prog1_id == 0)
    {
-       close(lake[0]);
-       dup2(lake[1], 1);
-       close(lake[1]);
+       dup2(lake1[1], 1);
+       close(lake1[0]);
+       close(lake1[1]);
+       close(lake2[0]);
+       close(lake2[1]);
        execlp(argv[1], argv[1], argv[2], argv[3], NULL);
        return 3;
    }
    prog2_id = fork();
    if (prog2_id == 0)
    {
-       dup2(lake[0], 0);
-       dup2(lake[1], 1);
-       close(lake[0]);
-       close(lake[1]);
+       dup2(lake1[0], 0);
+       dup2(lake2[1], 1);
+       close(lake1[0]);
+       close(lake1[1]);
+       close(lake2[0]);
+       close(lake2[1]);
        execlp(argv[4], argv[4], argv[5], argv[6], NULL);
        return 4;
    }
    wc_id = fork();
    if (wc_id == 0)
    {
-       close(lake[1]);
-       dup2(lake[0], 0);
-       close(lake[0]);
+       dup2(lake2[0], 0);
+       close(lake1[0]);
+       close(lake1[1]);
+       close(lake2[0]);
+       close(lake2[1]);
        execlp("wc", "wc", NULL);
        return 5;
    }
-   close(lake[0]);
-   close(lake[1]);
+   close(lake1[0]);
+   close(lake1[1]);
+   close(lake2[0]);
+   close(lake2[1]);
    waitpid(prog1_id, &status1, 0);
    waitpid(prog2_id, &status2, 0);
    waitpid(wc_id, &status3, 0);
